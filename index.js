8d3aecae3e92f76e408ddbd8133e077c9d7eb21c
Raiyan Yahya <raiyanyahyadeveloper@gmail.com>
2022-06-29 07:17:55 +0000

    test

---
+++ index.js
@@ -0,0 +1,16 @@
+import { Router } from 'itty-router';
+
+
+const router = Router()
+
+router.get("/pingpong", () => {
+    return new Response("OK")
+})
+router.all("*", () => new Response("404, not found!", {
+    status: 404
+}))
+
+addEventListener('fetch', (e) => {
+    const headerPayload = Object.fromEntries(e.request.headers)
+    e.respondWith(router.handle(e.request, headerPayload))
+})
